97fa9f400d8095bbb259e3ad555b7972724f8ca8
Danielle <danielle@kendallspace.net>
2023-02-02 18:46:46 -0500

    refactor Square and Board so game state is stored in Board instead of Square

--- client/src/components/board/index.js
+++ client/src/components/board/index.js
@@ -2,33 +2,25 @@ import React from "react";
 import Square from "../square";
 import "./index.css";
 
-// const Board = () => {
-//   return (
-//     <div className="board">
-//       <h1>Board</h1>
-//       <div className="square-container">
-//         <Square whichSquare="1" />
-//         <Square whichSquare="2" />
-//         <Square whichSquare="3" />
-//         <Square whichSquare="4" />
-//         <Square whichSquare="5" />
-//         <Square whichSquare="6" />
-//         <Square whichSquare="7" />
-//         <Square whichSquare="8" />
-//         <Square whichSquare="9" />
-//       </div>
-//     </div>
-//   );
-// };
-
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = { squares: Array(9).fill(null) };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
--- client/src/components/square/index.js
+++ client/src/components/square/index.js
@@ -2,19 +2,15 @@ import React from "react";
 import "./index.css";
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: null };
-  }
   render() {
     return (
       <button
         className="square"
         onClick={() => {
-          this.setState({ value: "X" });
+          this.props.onClick();
         }}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
